852d89c6914d3ef21a96b90c870fe183928e137f
Weijun826 <305403907+Weijun826@users.noreply.github.com>
2026-08-13 22:15:56 +0800

    feat: enable signed desktop updates

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,6 +12,8 @@ use tauri::Manager;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .setup(|app| {
             let app_data_directory = app.path().app_data_dir().map_err(|_| {
                 error::AppError::new(
